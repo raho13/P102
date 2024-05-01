22041f3889023fe67f3ad46abe0c44fb5873f138
Rahib <rahibrzayev2000@gmail.com>
2024-05-01 11:37:47 +0400

    js varables

---
+++ js intro/src/script/script.js
@@ -0,0 +1,23 @@
+// // console.log("salam")
+
+// var menimilkdeyiskenim = 2;
+// var menimikincideyiskenim = 5;
+// var menumumumicavabim = menimikincideyiskenim + menimilkdeyiskenim;
+
+// console.log(menimilkdeyiskenim);
+// console.log(menimikincideyiskenim);
+
+// console.log(menumumumicavabim);
+
+// // console.log(2 + 5);
+
+// alert("salam")
+
+var deyerbir = Number(prompt("toplanan 1"));
+var deyeriki = Number(prompt("toplanan 2"));
+var cem = deyerbir + deyeriki;
+console.log(deyerbir + deyeriki);
+// console.log(istifadecindenaldigimdeyer);
+
+// var mendeveloperemmi=confirm("sen developersen?")
+// console.log(mendeveloperemmi)
---
+++ js varables/script/index.js
@@ -0,0 +1,57 @@
+// Scope------------------------
+
+// Redeclaretion
+// var myValue = 9;
+// var myValue = "salam";
+
+// let myValue = 9;
+// let myValue = "salam";
+
+// Hoisting
+
+// TDZ temproray death zone
+
+// let myValue = 9;
+
+// let = "salam";
+
+// let="teszt"
+
+// var let = [2, 4, 6, 8];
+// console.log(let);
+// let.push("uhu");
+// console.log(let);
+
+// let = { name: "Rahib" };
+// console.log(let)
+// let test="test"
+// console.log(test)
+
+// const test = "salam";
+// test = "Aleykum";
+// console.log(test);
+
+// let myValue;
+//  myValue = 10;
+// console.log(myValue);
+
+// const myValue="";
+// console.log(myValue);
+
+let myString = "test string";
+myString = {
+  users: [
+    {
+      name: "Rahib",
+    },
+  ],
+};
+const myNumber = 10;
+const myBoolean = true; // false
+const myArray = [];
+const myObject = {};
+const myNullValue = null;
+const myUndefined = undefined;
+const myDateTime = new Date();
+
+console.log(myString);
