e4c474253775dc65776bbda43316ed2c285c9774
Rahib <rahibrzayev2000@gmail.com>
2024-05-06 12:27:59 +0400

    js operator && if else

---
+++ js operator/src/script/index.js
@@ -0,0 +1,46 @@
+// let variable = "salam";
+
+// let variable = 12 + 2;
+
+// let variable = 12 - 2;
+
+//  let variable = 12 % 2;
+
+//let variable = 2 ** 3
+
+// let variable = "4";
+// let test = "5";
+// variable = ++variable; // variable+1
+// variable = test + variable;
+// console.log(variable);
+// console.log(test);
+
+// let a = 2;
+// let b = 3;
+
+// // a += b; // a=a+b
+// // a -= b; // a=a-b
+// // a /= b; // a=a/b
+// // a *= b; // a=a*b
+// a %= b; // a=a%b
+// a **= b; //a=a**b
+// console.log(a);
+
+// let num1 = "5";
+// let num2 = 5;
+// console.log(num1 >= num2);
+// console.log(num1 > num2); // false
+// console.log(num1 === num2);
+
+// console.log(num1 !== num2);
+let num1 = 5;
+let num2 = 5;
+// && AND OPERATOR
+// || OR OPERATOR
+
+if (2 < 1) {
+} else if ((3 > 2 && 9 < 3) || 2 < 4 || 5 > 8 || 8 > 60) {
+  console.log("else if");
+} else {
+  console.log("test");
+}
