156120672736282147cf468ee6acbadc3e292f40
Rahib <rahibrzayev2000@gmail.com>
2024-05-08 12:44:59 +0400

    loops

---
+++ js loop switch case/src/script/index.js
@@ -0,0 +1,35 @@
+// let a = 2;
+
+// if (a > 19) {
+//   console.log("3 2-den boyukdur");
+// } else if (5 > a && a > 1) {
+//   console.log("ok");
+// }
+// else if (5 > a && a > 1) {
+//   console.log("ok");
+// }
+// else if (5 > a && a > 1) {
+//   console.log("ok");
+// }
+// else if (5 > a && a > 1) {
+//   console.log("ok");
+// } else {
+//   console.log("3 2-den boyuk deyil");
+// }
+
+// switch (2) {
+//   case 2:
+//     console.log(" a 2-e beraberdir");
+//     break;
+
+//   case 6:
+//     console.log(" a 6-e beraberdir");
+//     break;
+
+//   case 8:
+//     console.log(" a 8-e beraberdir");
+//     break;
+
+//   default:
+//     console.log("else");
+// }
---
+++ js loop switch case/src/script/loops.js
@@ -0,0 +1,67 @@
+// let array = [2, 5, 7];
+// console.log("ilk veziyyet", array);
+
+// array[0] = array[0] + 1;
+// array[1] = array[1] + 1;
+// array[2] = array[2] + 1;
+// array[3] = array[3] + 1;
+// array[4] = array[4] + 1;
+
+// console.log("son veziyyet", array);
+
+//-----------------FOR------------
+// console.log(array);
+
+// let i;
+// // array's length = 5
+
+// for (i = 0; i <= array.length - 1; i++) {
+//   array[i]++;
+// }
+// console.log(array);
+
+//------------------while----------
+// console.log(array)
+// let a = 0;
+
+// while (array.length - 1 >= a) {
+//   array[a]++;
+//   a++;
+// }
+// console.log(array)
+
+//---------------DO WHILE
+// console.log(array)
+// let a = 0;
+// do {
+//   array[a]++;
+//   a++;
+// } while (array.length - 1 >= a);
+// console.log(array)
+
+//-------------------for of
+
+// let myArr = [1, 3, 4, 6, 7];
+// let newArr = [];
+
+// for (const key of myArr) {
+//   newArr.push(key + 1);
+// }
+// console.log(newArr);
+//----------------for in------------------
+
+// console.log(person.name)
+// // for (const key in person) {
+// //   console.log(key);
+// //   console.log(person[key]); person.name || person.surname || person.testField
+// //   console.log("---------------------");
+// // }
+
+// let person = {
+//   name: "Rahib",
+//   5: "Ayan",
+// };
+// let array = [2, "test", 9];
+// console.log(array)
+// console.log(person[5]);
+
