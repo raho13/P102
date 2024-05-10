7906723eac28c1bb06fd68facf7a8031aafa7b5b
Rahib <rahibrzayev2000@gmail.com>
2024-05-10 11:54:50 +0400

    js functions

---
+++ js functions/src/scripts/index.js
@@ -0,0 +1,40 @@
+// Declaration function
+
+function topla(val, val2) {
+  console.log(val + val2);
+}
+
+// salamVer(13, 5);
+
+// Expression function // anonim functions
+// const salamVer = function (val) {
+//   return val
+// };
+
+// salamVer("salam ")
+// console.log(mesaj)
+// Arrow functions
+// let salamVer = (val, val2) => val + 10;
+
+// console.log(salamVer(22));
+// Invoked functions
+
+// let cavab = ((val) => {
+//   return val;
+// })("salam");
+// console.log(cavab);
+// function topla(value1, value2) {
+//   let sum = value1 + value2;
+//   return sum;
+// }
+// function cix(value1, value2) {
+//   let diff = value1 - value2;
+//   return diff;
+// }
+// let arr = [1, 2, 5, 7, 8];
+// let toplanandeyer = topla(arr, 6); //8
+// let cixilmisdeyer = cix(10, 5); //5
+
+// let cem = toplanandeyer + cixilmisdeyer;
+
+// console.log(cem);
