0feaded3afed3737fd342481243b258b90fc8c99
Rahib <rahibrzayev2000@gmail.com>
2024-05-27 11:59:54 +0400

    math & datetime & dom

---
+++ js Datetime & Math/src/script/index.js
@@ -0,0 +1,27 @@
+// let myDate = new Date("05.26.2014");
+// moment.locale("az-AZ");
+// let momentVal = moment(myDate).format("l");
+// console.log(momentVal);
+
+// let arr = [1, 3, 7, -5, 12, 8, 45, 23, -17];
+// let myVal = Math.random();
+// console.log(myVal);
+
+// let body = document.body;
+
+// let h1 = document.createElement("input");
+// h1.setAttribute("placeholder", "text");
+// // h1.value = "psw";
+// h1.style.borderColor = "green";
+
+// body.append(h1);
+
+// let paragraph = Array.from(document.getElementsByTagName("p"));
+// // paragraph.setAttribute("class", "paragraph");
+
+// function writeTag(tagname) {
+//   console.log(tagname.target.style.color);
+// }
+// paragraph.forEach((item) => {
+//   item.addEventListener("click", writeTag);
+// });
